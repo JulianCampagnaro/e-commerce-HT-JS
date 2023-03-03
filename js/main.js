d45bf49967520f5e440d887ab8655a71cb48910f
Julian Campagnaro <jcampagnaro123@gmail.com>
2023-03-03 20:59:03 -0300

    Correcciones varias, corrección de la validación de usuario

--- js/main.js
+++ js/main.js
@@ -20,18 +20,31 @@ if(modo === "dark" ) {
     document.body.classList.remove("dark");
 }
 
+//Para el scroll del header
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0 );
+});
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
 //SignUp usuario
 const formulario = document.getElementById("formularioSignUp");
-formulario.addEventListener ("submit", () => {
-    /* e.preventDefault(); */
-    const nombreUsuario = document.getElementById ("logNameSignUp").value;
-    const correoUsuario = document.getElementById("logEmailSignUp").value;
-    const passUsuario = document.getElementById("logPassSignUp").value;
 
-    localStorage.setItem ("usuario", nombreUsuario);
-    localStorage.setItem ("correo", correoUsuario);
-    localStorage.setItem ("pass", passUsuario);
-})
+    formulario.addEventListener ("submit", () => {
+        /* e.preventDefault(); */
+        const nombreUsuario = document.getElementById ("logNameSignUp").value;
+        const correoUsuario = document.getElementById("logEmailSignUp").value;
+        const passUsuario = document.getElementById("logPassSignUp").value;
+    
+        localStorage.setItem ("usuario", nombreUsuario);
+        localStorage.setItem ("correo", correoUsuario);
+        localStorage.setItem ("pass", passUsuario);
+    })
 
 
 //Inicio de sesión
@@ -48,6 +61,6 @@ formularioLogIn.addEventListener("submit", () => {
 })
 
 //Valido
-if (localStorage.getItem("correoLog") === correoRecuperado && localStorage.getItem ("passLog") === passRecuperado) {
+if (localStorage.getItem("correoLog") === correoRecuperado && localStorage.getItem("correoLog") != 0 && localStorage.getItem ("passLog") === passRecuperado && localStorage.getItem ("passLog") != null ) {
     console.log("Usted es bienvenido");
 }
